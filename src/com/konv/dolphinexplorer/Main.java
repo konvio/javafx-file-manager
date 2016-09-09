70035a79bf57247c0443250dd81469999ca1e9b4
konv <konvdev@gmail.com>
2016-09-09 18:18:46 +0300

    Create panes with FXML

--- src/com/konv/dolphinexplorer/Main.java
+++ src/com/konv/dolphinexplorer/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.konv.dolphinexplorer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,13 +9,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        primaryStage.setTitle("Dolphin Browser");
+        primaryStage.setScene(new Scene(root, 720, 435));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
