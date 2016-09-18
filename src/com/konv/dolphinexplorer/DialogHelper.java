cb0669ae4502505fdd278378a49738737065b09d
konv <konvdev@gmail.com>
2016-09-18 22:39:23 +0300

    Chnge notifications styling

--- src/com/konv/dolphinexplorer/DialogHelper.java
+++ src/com/konv/dolphinexplorer/DialogHelper.java
@@ -1,9 +1,13 @@
 package com.konv.dolphinexplorer;
 
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,10 +21,13 @@ public class DialogHelper {
 
     public static void showAlert(AlertType alertType, String title, String header, String content) {
         Alert alert = new Alert(alertType);
-        alert.initStyle(StageStyle.UTILITY);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("dolphin.png")));
+
         alert.showAndWait();
     }
 
@@ -40,15 +47,20 @@ public class DialogHelper {
         VBox.setVgrow(textArea, Priority.ALWAYS);
         alert.getDialogPane().setExpandableContent(new VBox(textArea));
 
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("dolphin.png")));
+
         alert.showAndWait();
     }
 
     public static boolean showConfirmationDialog(String title, String header, String content) {
         Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.initStyle(StageStyle.UTILITY);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
+
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("dolphin.png")));
 
         Optional<ButtonType> result = alert.showAndWait();
         return result.get() == ButtonType.OK;
@@ -57,7 +69,6 @@ public class DialogHelper {
     public static boolean showExpandableConfirmationDialog(String title, String header, String content,
                                                            String expandableContent) {
         Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.initStyle(StageStyle.UTILITY);
         alert.setTitle(title);
         alert.setHeaderText(header);
         alert.setContentText(content);
@@ -71,6 +82,9 @@ public class DialogHelper {
         VBox.setVgrow(textArea, Priority.ALWAYS);
         alert.getDialogPane().setExpandableContent(new VBox(textArea));
 
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("dolphin.png")));
+
         Optional<ButtonType> result = alert.showAndWait();
         return result.get() == ButtonType.OK;
     }
@@ -78,10 +92,12 @@ public class DialogHelper {
     @Nullable
     public static String showTextInputDialog(String title, String header, String content, String hint) {
         TextInputDialog dialog = new TextInputDialog(hint);
-        dialog.initStyle(StageStyle.UTILITY);
         dialog.setTitle(title);
         dialog.setHeaderText(header);
         dialog.setContentText(content);
+
+        Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("dolphin.png")));
 
         Optional<String> result = dialog.showAndWait();
         return result.isPresent() ? result.get() : null;
